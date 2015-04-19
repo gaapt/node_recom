955e2b18887b85c744597ea3a061c24548c269b5
gaapt <gabe@bluefather.com>
2015-04-19 00:07:36 -0300

    Update app.js

--- packages/users/app.js
+++ packages/users/app.js
@@ -16,37 +16,7 @@ MeanUser.register(function(app, auth, passport, database) {
   //We enable routing. By default the Package Object is passed to the routes
   MeanUser.routes(app, auth, database, passport);
 
-  //We are adding a link to the main menu for all authenticated users
-  // MeanUser.menus.add({
-  //     title: 'meanUser example page',
-  //     link: 'meanUser example page',
-  //     roles: ['authenticated'],
-  //     menu: 'main'
-  // });
-
   MeanUser.aggregateAsset('js', 'meanUser.js');
-
-  /**
-    //Uncomment to use. Requires meanio@0.3.7 or above
-    // Save settings with callback
-    // Use this for saving data from administration pages
-    MeanUser.settings({
-        'someSetting': 'some value'
-    }, function(err, settings) {
-        //you now have the settings object
-    });
-
-    // Another save settings example this time with no callback
-    // This writes over the last settings.
-    MeanUser.settings({
-        'anotherSettings': 'some value'
-    });
-
-    // Get settings. Retrieves latest saved settigns
-    MeanUser.settings(function(err, settings) {
-        //you now have the settings object
-    });
-    */
-
+ 
   return MeanUser;
 });
