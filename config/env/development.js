81baa6f49e3453da16dd10d41c9532b6614fe3f9
gaapt <gabe@bluefather.com>
2015-04-29 15:50:56 -0300

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://Nikita:65536@23.252.108.196:27017/recbase',
+  db: '<yourdb>',
   mongoose: {
     debug: true
   },
@@ -9,8 +9,8 @@ module.exports = {
     name: 'Base of Recommendations'
   },
   facebook: {
-    clientID: '915768598451160',
-    clientSecret: 'fb0865533ae0572940452db00c8ca1cb',
+    clientID: '<yourid>',
+    clientSecret: '<yoursecret>',
     callbackURL: 'http://serv.8-0.info/auth/facebook/callback'
   },
   twitter: {
@@ -24,8 +24,8 @@ module.exports = {
     callbackURL: 'http://serv.8-0.info/auth/github/callback'
   },
   google: {
-    clientID: '937308274165-9sdnsioc65tl7uceai0kg520u7t3pghu.apps.googleusercontent.com',
-    clientSecret: 'Cxlr6FZOoupN3ekdTlF2zzq2',
+    clientID: '<yourid>',
+    clientSecret: '<yoursecret>',
     callbackURL: 'http://serv.8-0.info/auth/google/callback'
   },
   linkedin: {
@@ -33,9 +33,9 @@ module.exports = {
     clientSecret: 'SECRET_KEY',
     callbackURL: 'http://serv.8-0.info/auth/linkedin/callback'
   },
-  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  emailFrom: 'SENDER EMAIL ADDRESS', 
   mailer: {
-    service: 'SERVICE_PROVIDER', // Gmail, SMTP
+    service: 'SERVICE_PROVIDER',
     auth: {
       user: 'EMAIL_ID',
       pass: 'PASSWORD'
