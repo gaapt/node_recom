56cbfebc18af592651d67a23b7a04d290d8731f3
gaapt <gabe@bluefather.com>
2015-07-26 22:50:38 -0300

    Update server.js
    explicit comments

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// Requires meanio
+
 var mean = require('meanio');
 
-// Creates and serves mean application
-mean.serve({ /*options placeholder*/ }, function(app, config) {
+mean.serve({ /*formal options*/ }, function(app, config) {
 	var port = config.https && config.https.port ? config.https.port : config.http.port;
 	console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ')');
 });
